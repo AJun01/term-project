7c7c1c3fdde0cd2311267d838af24f42bbc09c7d
Yujun Liu <yujunliu150@gmail.com>
2025-04-20 13:38:44 -0400

    created CRUD for mongoDB and updated a schema example commented

--- app/lib/store-weather.ts
+++ app/lib/store-weather.ts
@@ -1,6 +1,7 @@
 import { connectMongo } from './mongo-init';
 import Weather from '../models/Weather';
 
+//POST
 export async function storeWeather(zip: string, countryCode: string, data: any) {
   try {
     await connectMongo();
@@ -13,3 +14,39 @@ export async function storeWeather(zip: string, countryCode: string, data: any) 
     return { ok: false, error: 'DB insert failed' };
   }
 }
+
+//READ
+export async function getAllWeather() {
+    try {
+      await connectMongo();
+      const data = await Weather.find().sort({ fetchedAt: -1 });
+      return { ok: true, data };
+    } catch (err: any) {
+      console.error('read error:', err);
+      return { ok: false, error: 'DB read failed' };
+    }
+  }
+
+//Update
+export async function updateWeatherById(id: string, newData: any) {
+    try {
+        await connectMongo();
+        const updated = await Weather.findByIdAndUpdate(id, { data: newData }, { new: true });
+        return { ok: true, data: updated };
+    } catch (err: any) {
+        console.error('update error:', err);
+        return { ok: false, error: 'DB update failed' };
+    }
+}
+
+//delete
+export async function deleteWeatherById(id: string) {
+    try {
+      await connectMongo();
+      await Weather.findByIdAndDelete(id);
+      return { ok: true };
+    } catch (err: any) {
+      console.error('delete error:', err);
+      return { ok: false, error: 'DB delete failed' };
+    }
+  }
--- app/models/Weather.tsx
+++ app/models/Weather.tsx
@@ -1,5 +1,38 @@
 import mongoose from 'mongoose';
 
+//schema: so far, stores data like :
+// {"_id":{"$oid":"68052e47cf5fae0717f734c8"},
+// "zip":"02472","countryCode":"US",
+// "data":{
+// "coord":{"lon":{"$numberDouble":"-71.1773"},
+// "lat":{"$numberDouble":"42.37"}},
+// "weather":[{"id":{"$numberInt":"803"},
+// "main":"Clouds","description":"broken clouds","icon":"04d"}],
+// "base":"stations","main":{"temp":{"$numberDouble":"288.75"},
+// "feels_like":{"$numberDouble":"287.18"},
+// "temp_min":{"$numberDouble":"287.55"},
+// "temp_max":{"$numberDouble":"290.05"},
+// "pressure":{"$numberInt":"1020"},
+// "humidity":{"$numberInt":"31"},
+// "sea_level":{"$numberInt":"1020"},
+// "grnd_level":{"$numberInt":"1017"}},
+// "visibility":{"$numberInt":"10000"},
+// "wind":{"speed":{"$numberDouble":"10.29"},
+// "deg":{"$numberInt":"320"},
+// "gust":{"$numberDouble":"15.43"}},
+// "clouds":{"all":{"$numberInt":"75"}},
+// "dt":{"$numberInt":"1745169781"},
+// "sys":{"type":{"$numberInt":"2"},
+// "id":{"$numberInt":"2006202"},
+// "country":"US",
+// "sunrise":{"$numberInt":"1745142920"},
+// "sunset":{"$numberInt":"1745191884"}},
+// "timezone":{"$numberInt":"-14400"},
+// "id":{"$numberInt":"0"},"name":"Watertown",
+// "cod":{"$numberInt":"200"}},
+// "fetchedAt":{"$date":{"$numberLong":
+// "1745169991891"}},
+// "__v":{"$numberInt":"0"}}
 const weatherSchema = new mongoose.Schema({
   zip: String,
   countryCode: String,
