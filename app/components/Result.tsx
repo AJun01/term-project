aa0ad19b584ae83ed64e5dae8074918931db44e7
Kavya Verma <kavyaverma5601@gmail.com>
2025-04-26 10:23:04 -0400

    Added weather conditions Snow and Mist

--- app/components/Result.tsx
+++ app/components/Result.tsx
@@ -1,5 +1,7 @@
 import Clear from './conditions/Clear'
 import Clouds from './conditions/Clouds'
+import Snow from './conditions/Snow'
+import Mist from './conditions/Mist'
 import Default from './conditions/Default'
 
 export default function Result({ data }: { data: any }) {
@@ -8,6 +10,8 @@ export default function Result({ data }: { data: any }) {
   const conditionComponent = () => {
     if (condition.includes('clear')) return <Clear data={data} />
     if (condition.includes('clouds')) return <Clouds data={data} />
+    if (condition.includes('snow')) return <Snow data={data}/>
+    if (condition.includes('mist')) return <Mist data={data}/>
     return <Default data={data} />
   }
 
